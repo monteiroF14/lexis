d674d0ddc45c60ec203b3a3a7ea55fed5a19013a
mrjshzk <mrjsilva05@gmail.com>
2026-04-08 16:11:30 +0100

    begin refactor

--- src/session.js
+++ src/session.js
@@ -1,39 +1,25 @@
-export const session = {
-  loggedIn: false,
-  user: null,
-};
+export class Session {
+  #isValid = false;
+  get isValid() {
+    return this.#isValid;
+  }
 
-const USER_KEY = "user";
-const LOGGED_IN_KEY = "loggedIn";
-
-export function setUser(user) {
-  localStorage.setItem(USER_KEY, JSON.stringify(user));
-  session.user = user;
-
-  localStorage.setItem(LOGGED_IN_KEY, true);
-  session.loggedIn = true;
-}
-
-export function clearUser() {
-  localStorage.removeItem(USER_KEY);
-  session.user = null;
-
-  localStorage.removeItem(LOGGED_IN_KEY);
-  session.loggedIn = false;
-}
-
-if (localStorage.getItem(LOGGED_IN_KEY)) {
-  let user_json = localStorage.getItem(USER_KEY);
-  let user = JSON.parse(user_json);
-  setUser(user);
+  #user;
+  get user() {
+    return this.#user;
+  }
 }
 
 export class User {
-  constructor(name, email, password) {
-    this.username = name;
-    this.email = email;
-    this.password = password;
+  #username;
+  #email;
+  #password;
+
+  User(username, email, password) {
+    this.#username = username;
+    this.#email = email;
+    this.#password = password;
   }
 }
-export const defaultUsers = [new User("admin", "admin@admin", "admin")];
-localStorage.setItem("users", JSON.stringify(defaultUsers));
+
+export const session = Session.CreateSession();
