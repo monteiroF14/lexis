23c406d4cc9c99e0ae8a20e5683a8bf05d729e58
mrjshzk <mrjsilva05@gmail.com>
2026-05-14 18:59:36 +0100

    rollback vite config changes

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        dashboard: resolve(__dirname, "html/dashboard.html"),
-      },
-    },
   },
   base: "/lexis/",
 });
