ad09256c085e50bb380992ce833bd5b27db64d65
mrjshzk <mrjsilva05@gmail.com>
2026-05-18 02:09:00 +0100

    update vite config to add dashboard; update dashboard to not redirect to index

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
+
+const root = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   server: {
@@ -8,6 +12,12 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        dashboard: resolve(root, "html/dashboard.html"),
+      },
+    },
   },
   base: "/lexis/",
 });
