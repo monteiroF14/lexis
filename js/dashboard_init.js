7cae64dbcfd6b5ee63f05b760375ea4e9c79ebc4
mrjshzk <mrjsilva05@gmail.com>
2026-05-18 02:09:00 +0100

    update vite config to add dashboard; update dashboard to not redirect to index

--- js/dashboard_init.js
+++ js/dashboard_init.js
@@ -1,13 +1,23 @@
 import { SessionModel } from "./models/session_model.js";
+import { createAvatar } from "@dicebear/core";
+import { bigSmile } from "@dicebear/collection";
 
 const sessionModel = new SessionModel();
 const session = sessionModel.getSession();
 
 console.log(session);
-if (!session) {
-  window.location.href = "index.html";
-}
+// if no session, create here anonymous
 
-if (session.isAnonymous) {
-  // dar render só a algumas coisas
-}
+const avatar = createAvatar(bigSmile, {
+  accessories: [],
+  accessoriesProbability: 0,
+  eyes: ["cheery"],
+  hair: ["straightHair"],
+  hairColor: ["238d80"],
+  mouth: ["braces"],
+  skinColor: ["a47539"],
+  backgroundColor: ["transparent"],
+});
+
+const svg = avatar.toString();
+document.getElementById("user-avatar").src = svg;
