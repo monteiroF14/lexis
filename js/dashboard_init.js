18b41bf5846b5730f81ec5e7d583fa21381a16c4
mrjshzk <mrjsilva05@gmail.com>
2026-05-20 19:41:06 +0100

    add views for main tabs of dashboard

--- js/dashboard_init.js
+++ js/dashboard_init.js
@@ -3,12 +3,17 @@ import { createAvatar } from "@dicebear/core";
 import { bigSmile } from "@dicebear/collection";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import { DashboardModel } from "./models/dashboard_model.js";
+import { LevelsView } from "./views/levels_view.js";
+import { PdfView } from "./views/pdf_view.js";
+import { CustomizationView } from "./views/customization_view.js";
+import { StoreView } from "./views/store_view.js";
+import { SettingsView } from "./views/settings_view.js";
 
 const sessionModel = new SessionModel();
 const session = sessionModel.getSession();
 
 console.log(session);
-// if no session, create here anonymous
 
 const avatar = createAvatar(bigSmile, {
   accessories: [],
@@ -23,3 +28,11 @@ const avatar = createAvatar(bigSmile, {
 }).toDataUri();
 
 document.querySelector("#user-avatar").src = avatar;
+
+const dashboardModel = new DashboardModel();
+const levelsView = new LevelsView(dashboardModel);
+const pdfView = new PdfView(dashboardModel);
+const customizationView = new CustomizationView(dashboardModel);
+const storeView = new StoreView(dashboardModel);
+const settingsView = new SettingsView(dashboardModel);
+levelsView.render();
